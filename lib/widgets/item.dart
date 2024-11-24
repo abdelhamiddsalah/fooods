4db240289593d50f0039f3be1685eef20f19378e
abdelhamiddsalah <abdelhamidsalah222@gmail.com>
2024-11-24 16:48:45 +0200

    nav

--- lib/widgets/item.dart
+++ lib/widgets/item.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:fooods/constants/colors.dart';
 import 'package:fooods/models/itemModel.dart';
+import 'package:fooods/pages/correct.dart';
 import 'package:fooods/widgets/foodText.dart';
 
 class containerItem extends StatelessWidget {
@@ -11,49 +12,57 @@ class containerItem extends StatelessWidget {
  final bool isColor ;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 170,
-      height: 190,
-      decoration: BoxDecoration(
-          color:isColor? itemModel.backgroundColor:Colors.white , borderRadius: BorderRadius.circular(20)),
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Align(
-                alignment: Alignment.topRight,
-                child: Icon(
-                  Icons.favorite_border,
-                  color: mainColor,
-                )),
-            Image.asset(
-             itemModel.imageUrl,
-             height: 70,
-              width: 70,
-            ),
-            SizedBox(height: 9),
-            Foodtext(
-              data: itemModel.name,
-              size: 15,
-            ),
-            SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Foodtext(data: "\$ ${itemModel.price}", size: 15, color: mainColor),
-                Container(
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(30),
-                      color: mainColor.withOpacity(.3)),
+    return GestureDetector(
+      onTap: () {
+         Navigator.push(
+    context,
+    MaterialPageRoute(builder: (context) => const Correct()),
+  );
+      },
+      child: Container(
+        width: 170,
+        height: 190,
+        decoration: BoxDecoration(
+            color:isColor? itemModel.backgroundColor:Colors.white , borderRadius: BorderRadius.circular(20)),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Align(
+                  alignment: Alignment.topRight,
                   child: Icon(
-                    Icons.add,
+                    Icons.favorite_border,
                     color: mainColor,
-                  ),
-                )
-              ],
-            )
-          ],
+                  )),
+              Image.asset(
+               itemModel.imageUrl,
+               height: 70,
+                width: 70,
+              ),
+              SizedBox(height: 9),
+              Foodtext(
+                data: itemModel.name,
+                size: 15,
+              ),
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Foodtext(data: "\$ ${itemModel.price}", size: 15, color: mainColor),
+                  Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30),
+                        color: mainColor.withOpacity(.3)),
+                    child: Icon(
+                      Icons.add,
+                      color: mainColor,
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
