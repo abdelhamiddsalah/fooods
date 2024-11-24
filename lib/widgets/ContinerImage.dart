b0d9670cb095e59d2b1f18892bfb46ab67e0956f
abdelhamiddsalah <abdelhamidsalah222@gmail.com>
2024-11-24 16:44:29 +0200

    navs

--- lib/widgets/ContinerImage.dart
+++ lib/widgets/ContinerImage.dart
@@ -1,20 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:fooods/constants/colors.dart';
+import 'package:fooods/pages/correct.dart';
 
 class Continerimage extends StatelessWidget {
   const Continerimage({super.key, required this.image});
   final String image;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-          color: mainColor,
-          borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(20), topRight: Radius.circular(20))),
-      width: double.infinity,
-      height: MediaQuery.of(context).size.height * .55,
-      child: Center(
-        child: Image.asset(image),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+    context,
+    MaterialPageRoute(builder: (context) => const Correct()),
+  );
+      },
+      child: Container(
+        decoration: BoxDecoration(
+            color: mainColor,
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(20), topRight: Radius.circular(20))),
+        width: double.infinity,
+        height: MediaQuery.of(context).size.height * .55,
+        child: Center(
+          child: Image.asset(image),
+        ),
       ),
     );
   }
