0b680266604c0540759870e5f01c00a42f58de38
abdelhamiddsalah <abdelhamidsalah222@gmail.com>
2024-11-24 15:42:48 +0200

    modifychanges

--- lib/pages/items.dart
+++ lib/pages/items.dart
@@ -1,19 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:fooods/widgets/appbarinitems.dart';
+import 'package:fooods/widgets/foodText.dart';
 
 class Items extends StatelessWidget {
   const Items({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-            Appbarinitems(),
-            SizedBox(height: 20,),
-            Text('Helllo Kanta, What fruit Salad \n combo do you want today?'),
-            ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20,vertical: 10),
+      child: Scaffold(
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Appbarinitems(),
+              SizedBox(height: 20,),
+              Foodtext(data: 'Helllo Kanta, What fruit Salad \n combo do you want today?', size: 17)
+              ],
+          ),
         ),
       ),
     );
