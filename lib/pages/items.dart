61227feb9659a124901043dafff8e85e910040f3
abdelhamiddsalah <abdelhamidsalah222@gmail.com>
2024-11-24 15:20:04 +0200

    modify items

--- lib/pages/items.dart
+++ lib/pages/items.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:fooods/widgets/appbarinitems.dart';
 
 class Items extends StatelessWidget {
   const Items({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        children: [Appbarinitems()],
+      ),
+    );
   }
 }
