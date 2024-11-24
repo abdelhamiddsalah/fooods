0d6adb2d84b32dc429d0fe191b32e4412f36aa57
abdelhamiddsalah <abdelhamidsalah222@gmail.com>
2024-11-24 14:28:51 +0200

    item appbar

---
+++ lib/widgets/appbarinitems.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Appbarinitems extends StatelessWidget {
+  const Appbarinitems({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Icon(Icons.menu),
+        
+      ],
+    );
+  }
+}
